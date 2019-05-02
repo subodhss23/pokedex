6e6b72407ecaba1610fbdeddce31a1e345242739
Subodh Sharma <subodhss23@gmail.com>
2019-05-02 00:42:39 -0700

    adding up

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
+import './App.css';
+import Pokedex from './Pokedex';
 
 class App extends Component{
     render(){
         return (
-            
+            <div className="App">
+                <Pokedex />
+            </div>
         );
     }
 }
--- pokedex/src/Pokecard.js
+++ pokedex/src/Pokecard.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
+import './Pokecard.css';
+
+const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
 class Pokecard extends Component{
     render(){
+        let imgSrc = `${POKE_API}${this.props.id}.png`;
         return (
-            <div> 
-                <p>pokecard</p>
+            <div className="Pokecard"> 
+               <h1>{this.props.name}</h1> 
+               <img src = {imgSrc} alt={this.props.name} /> 
+               <div>{this.props.type}</div>
+               <div>{this.props.base_exp}</div>
             </div>
         );
     }
