31dd9db8b2f048b3697cf6a518ba58b5cfd56729
Subodh Sharma <subodhss23@gmail.com>
2019-05-01 23:52:00 -0700

    let the fun begin

---
+++ pokedex/src/Pokecard.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Pokecard extends Component{
+    render(){
+        return (
+            <div> 
+                <p>pokecard</p>
+            </div>
+        );
+    }
+}
+
+export default Pokecard;
